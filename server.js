9906dc9c6569ad19f207f7acbce67ad7c8845d5d
Gagandeep Singh <809gagan@gmail.com>
2021-06-17 18:38:11 +0530

    gg

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const path =  require('path')
 const port = process.env.PORT || 8080;
-const https = require('https');
-const fs = require('fs');
 const registerRoute = require('./serverRoutes/registerRoute')
 const loginRoute = require('./serverRoutes/loginRoute')
 const logoutRoute = require('./serverRoutes/logoutRoute')
@@ -45,12 +43,7 @@ app.get('*', (req,res) =>{
     res.sendFile((__dirname + '/build/index.html'));
 });
 
-const server = https.createServer({
-    key: fs.readFileSync(path.join(__dirname,'cert','key.pem')),
-    cert:fs.readFileSync(path.join(__dirname,'cert','cert.pem'))
-},app);
 
-
-server.listen(port,()=>{
-    console.log(`server is listening on port ` + port);
+app.listen(port ,()=>{
+    console.log("server is running on port number " + port);
 })
