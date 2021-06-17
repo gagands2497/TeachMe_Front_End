b4dffceeda6fa2777f86331997d559eff811fdc6
Gagandeep Singh <809gagan@gmail.com>
2021-06-17 18:55:56 +0530

    make it better

--- serverRoutes/logoutRoute.js
+++ serverRoutes/logoutRoute.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const crypto = require("crypto");
-const { Pool } = require('pg');
 require('dotenv').config();
+const { Pool } = require('pg');
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || 'postgresql://postgres:' + process.env.REACT_APP_DATABASE_PASSWORD + '@localhost:5432/' + process.env.REACT_APP_DATABASE_NAME,
-    ssl: process.env.DATABASE_URL ? true : false
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
 router.post('/',async(req,res)=>{
